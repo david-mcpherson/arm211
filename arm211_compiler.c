391ff5ec071c83784c25c25861e62327d0102dbe
david-mcpherson <davidharrymcpherson@gmail.com>
2022-06-21 17:50:31 -0700

    Created tempate for arm211_compiler.c

---
+++ arm211_compiler.c
@@ -0,0 +1,33 @@
+/*
+Compiler for CPEN 211 ISA (a subset of ARMv7)
+
+This program takes assembly files as arguments and creates machine code files
+in the format of the CPEN 211 ISA.
+
+author: 		david-mcpherson
+created: 		20-June-2022
+last updated:	20-June-2022
+*/
+
+/* PSEUDOCODE:
+main:
+For each file in argv:
+	For each line in file:
+		compileLine()
+
+compileLine(line):
+	int encoding;
+	operation = line[0:2]
+	switch(operation)
+		get the first five bits of the instruction
+		get the last 11 bits from a helper function
+
+
+
+*/
+
+
+int main(int argc, char** argv) {
+	return 0;
+}
+
