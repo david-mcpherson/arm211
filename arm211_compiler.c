f951b60fa9095c9e364d874c7c180df36941c937
david-mcpherson <davidharrymcpherson@gmail.com>
2022-06-25 15:36:52 -0700

    Removed merge conflict line from main file

--- arm211_compiler.c
+++ arm211_compiler.c
@@ -1,5 +1,4 @@
 /*
-<<<<<<< HEAD
 Compiles one or more files written in CPEN 211 ISA (simplified ARMv7)
 
 This program takes assembly files as arguments and creates machine code files
@@ -7,24 +6,9 @@ in the format of the CPEN 211 ISA.
 
 author: 		david-mcpherson
 created: 		20-June-2022
-last updated:	25-June-2022
+last updated:		25-June-2022
 */
 
-
-/* PSEUDOCODE:
-main:
-For each file in argv:
-	For each line in file:
-		compileLine()
-
-compileLine(line):
-	int encoding;
-	operation = line[0:2]
-	switch(operation)
-		get the first five bits of the instruction
-		get the last 11 bits from a helper function
-
- */
 
 #include "arm211_compiler.h"
 
