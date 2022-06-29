3c802bef28162c65ca58157a668184d165555c3f
david-mcpherson <davidharrymcpherson@gmail.com>
2022-06-28 20:40:43 -0700

    merged parser.h files into single tokens.h file

---
+++ tokens.h
@@ -0,0 +1,45 @@
+/**
+ * @brief Determines whether the move function is 
+ * a MOV_REG instruction
+ * 
+ * @param binary 
+ * @param assembly 
+ * @return true if the assembly is for MOV_REG
+ * @return false if the assembly is for MOV_IMM
+ */
+bool movReg(char* binary, char* assembly) {
+
+}
+
+
+/**
+ * @brief Set the Rn field of a binary instruction.
+ * 
+ * @param binary 
+ * @param assembly 
+ */
+void setRn(char* binary, char* assembly) {
+
+}
+
+
+/**
+ * @brief Set the 8-bit immediate field of an instruction.
+ * 
+ * @param binary 
+ * @param assembly 
+ */
+void setImmediate(char* binary, char* assembly) {
+
+}
+
+
+/**
+ * @brief Sets the Rd, shift and Rm fields of an instruction.
+ * 
+ * @param binary 
+ * @param assembly 
+ */
+void setRdShiftRm(char* binary, char* assembly) {
+
+}
